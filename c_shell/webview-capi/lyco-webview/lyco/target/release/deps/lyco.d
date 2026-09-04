7f6e4708df5c254338eb7c03e892ee78497de204
lilyco-42 <liuqingffffff@gmail.com>
2026-09-05 01:13:32 +0800

    fix: 恢复所有文件 (git rm 误操作后重新添加)

---
+++ c_shell/webview-capi/lyco-webview/lyco/target/release/deps/lyco.d
@@ -0,0 +1,22 @@
+D:\Code\minecraft\c_shell\webview-capi\lyco-webview\lyco\target\release\deps\lyco.d: src\main.rs src\../templates/main.c src\../templates/xmake.lua src\../templates/index.html src\../templates/main.py src\../templates/main.go src\../templates/main.rs src\../templates/main.ts src\../templates/Main.java src\../templates/main.zig src\../templates/Program.cs src\../templates/main.e.txt src\../templates/package.json src\../templates/android_manifest.xml src\../templates/build_apk.sh src\../templates/README.md src\../templates/.gitignore src\../templates/web.html
+
+D:\Code\minecraft\c_shell\webview-capi\lyco-webview\lyco\target\release\deps\lyco.exe: src\main.rs src\../templates/main.c src\../templates/xmake.lua src\../templates/index.html src\../templates/main.py src\../templates/main.go src\../templates/main.rs src\../templates/main.ts src\../templates/Main.java src\../templates/main.zig src\../templates/Program.cs src\../templates/main.e.txt src\../templates/package.json src\../templates/android_manifest.xml src\../templates/build_apk.sh src\../templates/README.md src\../templates/.gitignore src\../templates/web.html
+
+src\main.rs:
+src\../templates/main.c:
+src\../templates/xmake.lua:
+src\../templates/index.html:
+src\../templates/main.py:
+src\../templates/main.go:
+src\../templates/main.rs:
+src\../templates/main.ts:
+src\../templates/Main.java:
+src\../templates/main.zig:
+src\../templates/Program.cs:
+src\../templates/main.e.txt:
+src\../templates/package.json:
+src\../templates/android_manifest.xml:
+src\../templates/build_apk.sh:
+src\../templates/README.md:
+src\../templates/.gitignore:
+src\../templates/web.html:

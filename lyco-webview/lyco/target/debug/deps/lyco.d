cd3cb5fdb5d2c83aec636074a74ea4cef523678b
lilyco-42 <liuqingffffff@gmail.com>
2026-09-05 00:20:59 +0800

    feat: lyco CLI - 一键生成跨平台 WebView 项目 (Rust)
    命令:
      lyco new <name> [url]      # 生成项目 (Windows/Android/macOS/Linux/WASM)
      lyco build                 # 构建 (xmake/CMake)
      lyco run                   # 构建 + 运行
      lyco serve                 # WASM 模式 HTTP 服务
      lyco pack --target <t>     # 打包
    
    支持目标: windows, android, macos, linux, wasm

---
+++ lyco-webview/lyco/target/debug/deps/lyco.d
@@ -0,0 +1,5 @@
+D:\Code\minecraft\c_shell\webview-capi\lyco-webview\lyco\target\debug\deps\lyco.d: src\main.rs
+
+D:\Code\minecraft\c_shell\webview-capi\lyco-webview\lyco\target\debug\deps\lyco.exe: src\main.rs
+
+src\main.rs:
